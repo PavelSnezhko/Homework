80a23418bb42e7759fd4adb86b55b9521eabf5d5
pavel_snezhko <snezhko.dev@gmail.com>
2023-01-05 21:01:29 +0300

    update 2(3)

--- Python/Lesson2/2(3).py
+++ Python/Lesson2/2(3).py
@@ -1,12 +1,11 @@
 list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 numbers = None
-for objects in list:
+for index, objects in enumerate(list):
     try:
         numbers = int(objects)
     except ValueError:
         numbers = None
         pass
     if isinstance(numbers, int) == True:
-        print('"', objects, '"')
-    else:
-        print(objects)
+        list[index] = ('"' + objects + '"')
+print(list)
